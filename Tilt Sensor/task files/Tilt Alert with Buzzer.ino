7b3910149c319e21c4b2678afe3cfaaad37effd6
Stemup-EC <rohini@thestemup.com>
2025-03-19 15:27:28 +0530

    Create Tilt Alert with Buzzer.ino

---
+++ Tilt Sensor/task files/Tilt Alert with Buzzer.ino
@@ -0,0 +1,26 @@
+#define TILT_SENSOR_PIN 7  
+#define LED_PIN 9  
+#define BUZZER_PIN 10  
+
+void setup() {
+    pinMode(TILT_SENSOR_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    int sensorState = digitalRead(TILT_SENSOR_PIN);
+
+    if (sensorState == HIGH) {  
+        Serial.println("Tilt Detected!");
+        digitalWrite(LED_PIN, HIGH);
+        digitalWrite(BUZZER_PIN, HIGH);  
+    } else {
+        Serial.println("No Tilt Detected.");
+        digitalWrite(LED_PIN, LOW);
+        digitalWrite(BUZZER_PIN, LOW);
+    }
+    
+    delay(500);
+}
